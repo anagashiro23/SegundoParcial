273f26dae7cd734b04e66571d8659dbd13a8ba94
anagashiro <a2020116626@estudiantes.upsa.edu.bo>
2021-10-13 17:52:22 -0400

    Models Creado

---
+++ SegundoParcialAndresNagashiro/SegundoParcialAndresNagashiro/Models/Naipe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcialAndresNagashiro.Models
+{
+    public class Naipe
+    {
+        [Key]
+        public string naipe { get; set; }
+        [Display(Name = "Nombre de la carta")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "El nombre debe contener entre 3 a 60 caracteres")]
+        [Required(ErrorMessage = "El nombre del naipe es requerido")]
+        public string carta { get; set; }
+        [Display(Name = "nombre del naipe")]
+        public string nombre { get; set; }
+        [Url]
+        [Display(Name = "link de la imagen")]
+        public string imagen { get; set; }
+
+
+    }
+}
