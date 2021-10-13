2933e163e73a0503fc6be943df0cbdb9882db11c
anagashiro <a2020116626@estudiantes.upsa.edu.bo>
2021-10-13 18:06:26 -0400

    Crear Data y appsetttings.json

---
+++ SegundoParcialAndresNagashiro/SegundoParcialAndresNagashiro/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SegundoParcialAndresNagashiro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcialAndresNagashiro.Data
+{
+    public class AppDbContext : DbContext
+
+    {
+        public DbSet<Naipe> Naipes { get; set; } 
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
